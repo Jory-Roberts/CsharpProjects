7e60436fd5eaa74812fc593b3d669818900ce7b1
Jory-Roberts <robertsjory13@gmail.com>
2024-01-28 17:48:01 -0500

    classes tutorial

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,13 +1,34 @@
-﻿int[] inventory = { 200, 450, 700, 175, 250 };
+using Classes;
 
-int sum = 0;
-int bin = 0;
+var account = new BankAccount("Julie", 1000);
+Console.WriteLine(
+    $"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance"
+);
 
-foreach (int items in inventory)
-{
-	sum += items;
-	bin++;
-	Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
-}
+account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+Console.WriteLine(account.Balance);
+account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+Console.WriteLine(account.Balance);
 
-Console.WriteLine($"We have {sum} items in inventory");
+// BankAccount invalidAccount;
+// try
+// {
+//     account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+// }
+// catch (InvalidOperationException e)
+// {
+//     Console.WriteLine("Exception caught trying to overdraw.");
+//     Console.WriteLine(e.ToString());
+// }
+// try
+// {
+//     invalidAccount = new BankAccount("Invalid", -55);
+// }
+// catch (ArgumentOutOfRangeException e)
+// {
+//     Console.WriteLine("Exception caught creating account with negative balance");
+//     Console.WriteLine(e.ToString());
+//     return;
+// }
+
+Console.WriteLine(account.GetAccountHistory());
